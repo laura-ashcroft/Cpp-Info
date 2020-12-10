01e71624a2eecf40d87df0eae86e8c03ddbdb2af
Laura Ashcroft <laura.ashcroft14@gmail.com>
2020-12-10 17:47:50 +0000

    C++ info

---
+++ pop.cpp
@@ -0,0 +1,20 @@
+// Description
+// pop() function is used to remove an element from the top of the stack(newest element in the stack). The element is removed to the stack container and the size of the stack is decreased by 1.
+
+#include<stack>
+#include<iostream>
+
+int main(){
+    std::stack<int> mystack;
+
+    mystack.push(0);    //pushing elements using push()
+    mystack.push(1);
+    mystack.push(2);
+
+    while (!mystack.empty()) {
+        std::cout << ' ' << mystack.top();
+        mystack.pop();  //deleting elements using pop()
+    }
+
+    return 0;
+}
